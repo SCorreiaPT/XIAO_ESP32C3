4b0cf4390b108d2fb617acca8a9bcf2fe9323686
Sergio Duarte Correia <scorreia@ipportalegre.pt>
2023-07-31 20:23:58 +0100

    Create HardwareTest.ino

---
+++ code/HardwareTest/HardwareTest.ino
@@ -0,0 +1,84 @@
+/************************************************************************************/
+/* Arduino Script for testing Seeed Studio XIAO ESP32-C3 Module								*/
+/*																					*/
+/* RISC-V CPU, 4MB Flash memory, 400 KB SRAM.		*/
+/*																					*/
+/* Developed at:																	*/
+/* Laboratory of Electronics and Instrumentation,									*/
+/* Portalegre Polytechnic University, Portalegre, Portugal							*/
+/*																					*/
+/* @author Prof. Sergio D. Correia													*/
+/* @date July, 2023																	*/
+/* @version 0.0																		*/
+/* @file HardwareTest.ino															*/
+/* @see https://sites.google.com/ipportalegre.pt/scorreia/home						*/
+/*																					*/
+/************************************************************************************/
+
+#define SerialDEBUG		Serial
+#define UART_BAUD     115200
+#define MAXNUM        1000000
+
+unsigned long start, stop, t;
+unsigned long i = 2;
+
+void setup() {
+
+  SerialDEBUG.begin(UART_BAUD);
+
+	// Model Information
+	SerialDEBUG.println();
+	SerialDEBUG.println("====================================================");
+	SerialDEBUG.println("***                   ESP32-C3                   ***");
+	SerialDEBUG.printf("*** ESP32 Model %s with %dx cores at %dMHz ***\n",ESP.getChipModel(),ESP.getChipCores(),ESP.getCpuFreqMHz());
+	SerialDEBUG.println("***                 Firmware v0.0                ***");
+	SerialDEBUG.println("====================================================");
+	SerialDEBUG.println();
+
+	// Memory Information
+	SerialDEBUG.println("====================================================");
+	SerialDEBUG.printf("Flash Memory: %d, at %d MHz \n",ESP.getFlashChipSize(),ESP.getFlashChipSpeed()/1000000);
+	SerialDEBUG.println("====================================================");
+	SerialDEBUG.printf("Internal RAM  size   : %u \n", ESP.getHeapSize());
+	SerialDEBUG.printf("Internal RAM  free   : %u \n", ESP.getFreeHeap());
+	SerialDEBUG.println("====================================================");
+	SerialDEBUG.println();
+
+  start = millis(); 
+
+}
+
+void loop() {
+
+  
+
+  boolean p = isPrime(i);
+  i++;
+
+  if(i>MAXNUM){
+    stop = millis();
+    t = stop - start; 
+    i = 2;
+
+    SerialDEBUG.print("DONE in ");
+    SerialDEBUG.print(t);
+    SerialDEBUG.println(" ms");
+
+    start = millis();
+  }
+    
+}
+
+
+boolean isPrime(unsigned long x) {
+  boolean prime = true;
+  
+  for(int i = 2; i <= x/2; i++) {     // Loop up to half the number
+    if(x % i == 0) {                  // Check if divisible
+      prime = false;                  // NOT prime
+      break;
+    }
+  }
+  
+  return prime;
+}
